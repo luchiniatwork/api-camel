ea0dcdd84fb94ab70abadce9994e7e4141ca6374
Tiago Luchini <luchini@work.co>
2014-08-29 00:29:04 -0700

    Fixtures loader as a chained pattern

--- test/fixtures.js
+++ test/fixtures.js
@@ -4,19 +4,22 @@ var nock = require('nock');
 var path = require('path');
 var fs = require('fs');
 
-module.exports = {
-  load: function (names) {
-    var bypassFixtures = process.env.BYPASS_FIXTURES;
-    debug('bypassing?', bypassFixtures);
-    names = (typeof names === 'string') ? [names] : names;
-    for (var idx in names) {
-      var fp = path.join('test', 'fixtures', names[idx] + '.js');
-      try {
-        debug('attempting to load fixture', names[idx], 'at', fp);
-        require('../' + fp);
-      } catch (e) {
-        debug('fixture not found');
-      }
+var loader = function (names) {
+  var bypassFixtures = process.env.BYPASS_FIXTURES;
+  debug('bypassing?', bypassFixtures);
+  names = (typeof names === 'string') ? [names] : names;
+  for (var idx in names) {
+    var fp = path.join('test', 'fixtures', names[idx] + '.js');
+    try {
+      debug('attempting to load fixture', names[idx], 'at', fp);
+      require('../' + fp);
+    } catch (e) {
+      debug('fixture not found');
     }
   }
+  return loader;
+};
+
+module.exports = {
+  load: loader
 };
